a0f9f216dbf84065b771f15dbe61ab4c07a13533
inessm9 <inessemitiel3@gmail.com>
2025-05-26 11:17:53 +0200

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -1,20 +1,31 @@
-class Empleado {
-    String nombre;
-    String cargo;
-    double salario;
+public class Empleado {
+    private final String nombre;
+    private final String cargo;
+    private final double salario;
 
+    /*
+     * metodo empleado para recoger las variables
+     */
     public Empleado(String nombre, String cargo, double salario) {
         this.nombre = nombre;
         this.cargo = cargo;
         this.salario = salario;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
     @Override
     public String toString() {
-        return "Empleado{" +
-                "nombre='" + nombre + '\'' +
-                ", cargo='" + cargo + '\'' +
-                ", salario=" + salario +
-                '}';
+        return String.format("Empleado{nombre='%s', cargo='%s', salario=%.2f}", nombre, cargo, salario);
     }
 }
